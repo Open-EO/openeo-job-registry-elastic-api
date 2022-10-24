6ee7663fe381b26b1a8739a4b488a06af8b92296
janssenb <bram.janssen@vito.be>
2022-10-24 15:54:14 +0200

    feat(fd-5452): setup of api config

--- src/config/config/config.service.ts
+++ src/config/config/config.service.ts
@@ -1,22 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import * as Convict from 'convict';
-import {AppConfig, Schema} from "../schema/config.schema";
+import { AppConfig, Schema } from '../schema/config.schema';
 import * as dotenv from 'dotenv';
 
 @Injectable()
 export class ConfigService {
-    config: Convict.Config<AppConfig>;
+  config: Convict.Config<AppConfig>;
 
-    constructor() {
-        this.config = Convict(Schema);
-        const dotEnvFile = dotenv.config().parsed;
-        if (dotEnvFile) {
-            this.config.load(dotenv.config().parsed);
-        }
-        this.config.validate({ allowed: 'strict' });
-    }
+  constructor() {
+    dotenv.config();
+    this.config = Convict(Schema);
+    this.config.validate({ allowed: 'strict' });
+  }
 
-    get(configName: string) {
-        return this.config.get(configName);
-    }
+  get(configName: string) {
+    return this.config.get(configName);
+  }
 }
--- src/config/schema/config.schema.ts
+++ src/config/schema/config.schema.ts
@@ -1,15 +1,17 @@
 import * as Convict from 'convict';
 
 export interface AppConfig {
-    port: number
-};
+  port: number;
+}
 
 export const Schema: Convict.Schema<AppConfig> = {
+  general: {
     port: {
-        doc: 'Port that is used to bind the API',
-        format: 'port',
-        default: 3000,
-        env: 'PORT',
-        arg: 'port'
-    }
+      doc: 'Port that is used to bind the API',
+      format: 'port',
+      default: 3000,
+      env: 'PORT',
+      arg: 'port',
+    },
+  },
 };
