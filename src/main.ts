e0e966b013f0dc45568937d402cab265f6fab0ea
janssenb <bram.janssen@vito.be>
2022-10-24 16:04:32 +0200

    feat(fd-5452): setup of swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from './config/config/config.service';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { version } from '../package.json';
 
-async function bootstrap() {
+const setupDocs = (app) => {
+  const config = new DocumentBuilder()
+    .setTitle('OpenEO Job Tracker API')
+    .setDescription(
+      'API endpoint for tracking jobs that are executed within OpenEO',
+    )
+    .setVersion(version)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+};
+
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
   const configService: ConfigService = app.get(ConfigService);
+  setupDocs(app);
   await app.listen(configService.get('general.port'));
-}
+};
+
 bootstrap();
