5d3ea9b20cd4532d2c458c21c2b4dc12687ab7d3
bramjanssen <bram.janssen@vito.be>
2024-05-24 11:50:24 +0200

    fix: set min level to info if debug is disabled

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { v4 as uuidv4 } from 'uuid';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (config: ConfigService) => {
-        const level = config.get('general.debug') ? 'debug' : 'warn';
+        const level = config.get('general.debug') ? 'debug' : 'info';
         return {
           level,
           pinoHttp: {
